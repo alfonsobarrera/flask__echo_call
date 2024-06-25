40b431207c550107f270d4323aa698fd0bb55c0a
alfonsobarrera <alfonsobarrera_1@hotmail.com>
2024-06-25 00:14:05 -0600

    changes on 25/6/2024

--- server.py
+++ server.py
@@ -1,30 +1,22 @@
 from flask import Flask, request, jsonify
 import requests
+import whois
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    data={'page':'home'}
-    return jsonify(data)
+@app.route("/search/<domain>", methods=['GET', 'POST'])
+def home(domain):
+	assert domain == request.view_args['domain']
+	print(domain)
+	return jsonify(search(domain))
 
 
-@app.route("/search", methods=['GET', 'POST'])
-def search():
-    data={'endpoint':'search'}
-    if request.method == 'POST':
-        domain=request.form['domain']
-        url='https://{}/'.format(domain)
-        try:
-            resp=requests.get(url, verify=False)
-            data={'endpoint':'search','domain':'{}'.format(url),'status_code':'{}'.format(resp.status_code)}
-        except Exception as e:
-            error=str(e)
-            data={'page':'search','error':'{}'.format(error)}
-    if request.method == 'GET':
-        data={'page':'search','error':'Method should be POST'}
-        
-    return jsonify(data)
+
+def search(domain):
+	search_result=whois.whois(domain)
+	return(search_result)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
